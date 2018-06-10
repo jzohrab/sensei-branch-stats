83986c7f6f8650cc93639bf79257e6e656030c6e
Jeff Zohrab <jzohrab@gmail.com>
2018-06-10 12:25:06 -0400

    wip branch growth per feature

--- branch_line_chart.rb
+++ branch_line_chart.rb
@@ -1,9 +1,8 @@
 require 'date'
+require 'pp'
 
 $stderr.sync = true
 $stdout.sync = true
-
-today = Date::today
 
 
 
@@ -42,95 +41,54 @@ def parse_commit_data(s)
 
 end
 
+
+class Date
+  def yyyymmdd()
+    return self.strftime("%Y-%m-%d")
+  end
+end
+
+
+def build_growth_line(commits)
+  return {} if commits.size() == 0
+
+  linecount_per_day = Hash.new { |h, k| h[k] = 0 }
+  commits.each { |c| linecount_per_day[c[:date]] += c[:linecount] }
+  dates = commits.
+          map { |c| c[:date] }.uniq.
+          map { |s| Date.strptime(s, "%Y-%m-%d") }
+
+  size_per_day = {
+    (dates.min - 1).yyyymmdd() => 0
+  }
+
+  (dates.min..Date::today).to_a.each do |d|
+    size_per_day[d.yyyymmdd()] = size_per_day[(d-1).yyyymmdd()] + linecount_per_day[d.yyyymmdd()]
+  end
+  size_per_day
+end
+
+
+def get_growth_over_time(base, branch)
+  puts '-' * 20
+  puts branch
+  all_commits = get_all_commits(base, branch)
+  puts all_commits
+  build_growth_line(all_commits)
+end
+
 branch = 'origin/feature/1418308_move-redirect-bar-to-websrc'
 
-Dir.chdir('../klick-genome') do |d|
-  puts "#{d}"
-  all_commits = get_all_commits('origin/develop', branch)
-  puts all_commits
-  # growth_over_time = build_growth_line(all_commits)
-end
-
-return
-
-#####################
-
-def from_today(s)
-  return 0 if s.nil?
-  d = Date.strptime(s,"%Y-%m-%d")
-  return (Date::today - d).to_i
-end
-
-
-def branch_stats(base_branch, b)
-  reverse_commits = `git log #{base_branch}..#{b} --reverse --date=short --pretty=format:%cd`.split("\n")
-  ret = {
-    :ahead => reverse_commits.size,
-    :first_commit_date => reverse_commits[0],
-    :age => from_today(reverse_commits[0]),
-    :last_commit_date => reverse_commits[-1],
-    :stale => from_today(reverse_commits[-1])
-  }
-
-  ret.merge! get_diff_numstat_summary(base_branch, b)
-
-  ret
-end
-
-
-def get_diff_numstat_summary(base_branch, b)
-  cmd = "git diff #{base_branch}...#{b} --numstat"
-  # puts cmd
-  # return
-  
-  # outputs "additions  deletions  filename" (tab-separated)
-  raw = `#{cmd}`.split("\n")
-  # puts raw.inspect
-  data = raw.map do |line|
-    add, delete, filename = line.split("\t")
-    [add.to_i, delete.to_i, filename]
-  end
-
-  additions = data.map { |d| d[0] }.reduce(0, :+)
-  deletions = data.map { |d| d[1] }.reduce(0, :+)
-
-  return {
-    :branch => b,
-    :diff_stats => data,
-    :additions => additions,
-    :deletions => deletions,
-    :linecount => additions + deletions,
-    :file_count => data.size()
-  }
-
-end
 
 Dir.chdir('../klick-genome') do |d|
-  puts "#{d}"
   remote_branches = `git branch -r`.split("\n").map { |b| b.strip }
   features = remote_branches.select { |b| b =~ /feature/ }
 
-  n = 0
-  data = features.map do |b|
-    n += 1
-    $stderr.puts " #{b} (#{n} of #{features.size})"
-    branch_stats('origin/develop', b)
+  data = {}
+  features.each do |f|
+    data[f] = get_growth_over_time('origin/develop', f)
   end
 
-  puts "Done\n\n"
-  data.
-    select { |d| d[:linecount] > 0 }.
-    select { |d| d[:stale] <= 20 }.
-    sort { |a, b| a[:linecount] <=> b[:linecount] }.
-    reverse.
-    each_with_index do |b, i|
-    puts [ "B#{i + 1}", b[:branch].gsub('origin/feature/', ''), b[:age], b[:file_count], 'feature', b[:linecount] ].join('|')
-  end
-
+  pp data
 end
 
-
-# Get all active branches
-# Get all changes per commit date - a bunch of diffs only
-# 
-# 

24c13e0cdf5e75592f0ba0546fb775ed66eeef91
Jeff Zohrab <jzohrab@gmail.com>
2018-06-11 21:10:49 -0400

    WIP pass token in ctor

--- Rakefile
+++ Rakefile
@@ -1,22 +1,11 @@
 require 'yaml'
 require_relative 'lib/github_graphql'
 
-desc "push tokens into ENV"
-task :configure_ENV do
-  key = GitHubGraphQL::TOKEN
-  github_creds_file = File.join(File.dirname(__FILE__), 'github_creds.yml')
-  if (ENV[key].nil? && File.exist?(github_creds_file)) then
-    puts "Setting #{key}"
-    hsh = YAML.load_file(github_creds_file)
-    token = hsh[key]
-    ENV[key] = token
-  end
-end
 
 desc "Dump the GraphQL schema"
-task :dump_schema => [:configure_ENV] do
+task :dump_schema do
   puts "Dumping schema"
-  g = GitHubGraphQL.new()
+  g = GitHubGraphQL.new(GitHubGraphQL.auth_token())
   g.dump_schema(GitHubGraphQL::SCHEMAFILE)
   unauthorized = (File.read(GitHubGraphQL::SCHEMAFILE) =~ /401 Unauthorized/)
   raise "Unauthorized schema dump.  Missing credentials?" if unauthorized
--- lib/github_graphql.rb
+++ lib/github_graphql.rb
@@ -4,30 +4,56 @@ require "graphql/client/http"
 class GitHubGraphQL
 
   TOKEN = 'GITHUB_GRAPHQL_API_TOKEN'
+  CREDSFILE = File.join(File.dirname(__FILE__), 'github_creds.yml')
   SCHEMAFILE = File.join(File.dirname(__FILE__), 'schema.json')
-  
-  def get_configured_http()
-    return GraphQL::Client::HTTP.new("https://api.github.com/graphql") do
-      def headers(context)
-        b = "bearer #{ENV[GitHubGraphQL::TOKEN]}"
-        {
-          "User-Agent" => "My Client",
-          "Authorization" => b
-        }
-      end
+
+  # Reads the auth token from config file.  If not available, falls
+  # back to environment.
+  def self.auth_token()
+    token = nil
+    if File.exist?(CREDSFILE) then
+      puts "#{CREDSFILE} found"
+      hsh = YAML.load_file(CREDSFILE)
+      token = hsh[GitHubGraphQL::TOKEN]
     end
+    token = ENV[GitHubGraphQL::TOKEN] if token.nil?
+    raise "Missing token in env and creds file" if token.nil?
+    token
   end
+
+
+  def initialize(token)
+    @token = token
+  end
+
+
+  def get_configured_http()
+    h = GraphQL::Client::HTTP.new("https://api.github.com/graphql")
+    headers = <<HERE
+      def headers(context)
+      {
+        "User-Agent" => "My Client",
+        "Authorization" => "bearer #{@token}"
+      }
+end
+HERE
+    h.instance_eval(headers)
+    h
+  end
+
 
   def get_schema()
     http = get_configured_http()
     schema = GraphQL::Client.load_schema(http)
   end
 
+
   def dump_schema(f)
     http = get_configured_http()
     GraphQL::Client.dump_schema(http, f)
   end
-  
+
+
   def client()
     http = get_configured_http()
     schema = nil

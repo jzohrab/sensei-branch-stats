3b80c8c70dd5503c3701d8d896f6e3813d50932d
Jeff Zohrab <jzohrab@gmail.com>
2018-06-14 00:26:25 -0400

    wip

--- gen_reports.rb
+++ gen_reports.rb
@@ -10,9 +10,13 @@ require "active_support/notifications"
 
 def put_markdown_table(ostream, headings, rows, order_by = nil, ascending = true)
   print_lin = lambda { |a| ostream.puts "| #{a.join(' | ')} |" }
-  ostream.puts "<div style=\"font-size:10px\">"
-  ostream.puts  # Space after div is required for wiki
-  print_lin.call(headings.map { |h| h.to_s.gsub('_', ' ') })
+  # ostream.puts "<div style=\"font-size:10px\">"
+  # ostream.puts  # Space after div is required for wiki
+  print_headings =
+    headings.
+    map { |h| h.to_s.gsub('_', ' ') }
+
+  print_lin.call(print_headings)
   print_lin.call(headings.map { |h| '---' })
 
   rows.sort! { |a, b| a[order_by] <=> b[order_by] } if order_by
@@ -21,7 +25,7 @@ def put_markdown_table(ostream, headings, rows, order_by = nil, ascending = true
   rows.each do |row|
     print_lin.call(headings.map { |h| row[h] })
   end
-  ostream.puts "</div>"
+  # ostream.puts "</div>"
 end
 
 
@@ -37,38 +41,84 @@ class String
 end
 
 
-def symbolize(s)
-  # Hack!  I loaded the images into the wiki and noted their IDs.
-  img = lambda do |name, f|
-    "![#{name}](https://files.wiki.senseilabs.com/#{f})"
-  end
-  green = img.call('green', '85zxpAMOCaW/aba10f31-2cee-499d-ac31-e3fdd4188c04')
-  red = img.call('red', '85zxpAMOCaW/5300c533-ef76-45c6-a601-787f7da8bfac')
-  yellow = img.call('yellow', '85zxpAMOCaW/d1c03456-eaf2-47ae-95b6-0f5a080d69da')
+class StatusImage
+  # Hack!  I loaded images into the wiki and noted their IDs.
+  @@hsh = {
+    green: '85zxpAMOCaW/aba10f31-2cee-499d-ac31-e3fdd4188c04',
+    red: '85zxpAMOCaW/5300c533-ef76-45c6-a601-787f7da8bfac',
+    yellow: '85zxpAMOCaW/d1c03456-eaf2-47ae-95b6-0f5a080d69da'
+  }
 
-  case s
-  when 'yes', 'good', 'APPROVED', 'SUCCESS', 'true', true
-    green
-  when 'no', 'bad', 'CHANGES_REQUESTED', 'FAILURE', 'false', false
-    red
-  when 'PENDING', nil
-    yellow
-  else
-    s
+  def initialize(status)
+    @status = status
+    path = @@hsh[status]
+    raise "Bad status #{status}" unless path
+    @img = "![#{status}](https://files.wiki.senseilabs.com/#{path})"
   end
+
+  def to_s()
+    @img
+  end
+
+  @@green = StatusImage.new(:green)
+  @@red = StatusImage.new(:red)
+  @@yellow = StatusImage.new(:yellow)
+
+  def self.Green
+    @@green
+  end
+  def self.Yellow
+    @@yellow
+  end
+  def self.Red
+    @@red
+  end
+
+  def self.symbolize(s)
+    case s
+    when 'yes', 'good', 'APPROVED', 'SUCCESS', 'true', true
+      StatusImage.Green
+    when 'no', 'bad', 'CHANGES_REQUESTED', 'FAILURE', 'false', false
+      StatusImage.Red
+    when 'PENDING', nil
+      StatusImage.Yellow
+    else
+      s
+    end
+  end
+
+  def self.worst_of(string_array)
+    syms = string_array.map { |s| self.symbolize(s) }
+    [StatusImage.Red, StatusImage.Yellow, StatusImage.Green].each do |s|
+      return s if syms.include?(s)
+    end
+    nil
+  end
+
+end
+
+
+def authors(data)
+  data[:commits][:authors].
+    map { |a| a.gsub(/@.*/, '') }.
+    join('<br />')
 end
 
 
 def gen_branches(data, filename)
-  headings = [:branch, :authors, :last_commit, :change]
+  headings = [:branch, :ci, :authors, :last_commit, :change]
   create_row = lambda do |d|
     b = d[:branch]
     c = d[:commits]
     row = {
-      branch: "#{symbolize(b[:status])} #{b[:name]}",
-      authors: d[:commits][:authors].join('<br />'),
+      ci: StatusImage.symbolize(b[:status]),
+      branch: b[:name],
+      authors: authors(d),
       last_commit: b[:last_commit],
-      change: "+#{c[:additions]} / -#{c[:deletions]}"
+      change: "+#{c[:additions]} / -#{c[:deletions]}",
+
+      # Add a sort key, dup it to ensure later mutation doesn't break it.
+      last_commit_SORT_KEY: b[:last_commit].dup
     }
     row[:last_commit].flag_if!(b[:last_commit_age] > 20)
     row[:change].flag_if!(c[:additions] + c[:deletions] > 500)
@@ -83,41 +133,42 @@ def gen_branches(data, filename)
   File.open(filename, 'w') do |f|
     f.puts "# Branches"
     f.puts
-    put_markdown_table(f, headings, rows, :last_commit, true)
+    put_markdown_table(f, headings, rows, :last_commit_SORT_KEY, false)
   end
   puts "Wrote #{filename}"
 end
 
 
 def gen_pull_requests(data, filename)
-###  headings = [:pull_request, :branch, :authors, :created, :age, :status, :mergeable, :reviews]
-###  create_row = lambda do |d|
-###    pr = d[:pr]
-###    r = pr.slice(*headings)
-###    r[:pull_request] = "[#{pr[:number]}: #{pr[:title]}](#{pr[:url]})"
-###    r[:authors] = d[:commits][:authors].join('<br />')
-###    r[:status] = symbolize(d[:branch][:status])
-###    r[:reviews] =
-###      d[:pr][:reviews].
-###      map { |r| r[:status] }.
-###      select { |r| r != 'COMMENTED' }.
-###      map { |r| symbolize(r) }.
-###      join('')
-###    r
-###  end
-###
-###  rows =
-###    data.
-###    select { |d| d[:pr].keys().size() > 0 }.
-###    map { |d| create_row.call(d) }
-###
-###  File.open(filename, 'w') do |f|
-###    f.puts "# Pull Requests"
-###    f.puts
-###    put_markdown_table(f, headings, rows, :age, false)
-###  end
-###  puts "Wrote #{filename}"
-###
+  headings = [:pull_request, :branch, :authors, :created, :ci, :mergeable, :reviews]
+  create_row = lambda do |d|
+    pr = d[:pr]
+    revs = pr[:reviews].map { |r| r[:status] }.select { |r| r != 'COMMENTED' }
+    row = {
+      pull_request: "[#{pr[:number]}: #{pr[:title]}](#{pr[:url]})",
+      branch: d[:branch][:name],
+      authors: authors(d),
+      created: pr[:created],
+      ci: StatusImage.symbolize(d[:branch][:status]),
+      mergeable: StatusImage.symbolize(pr[:mergeable]),
+      reviews: StatusImage.worst_of(revs),
+      age_SORT_KEY: pr[:age]
+    }
+    row[:created].flag_if!(pr[:age] > 20)
+    row
+  end
+
+  rows =
+    data.
+    select { |d| d[:pr].keys().size() > 0 }.
+    map { |d| create_row.call(d) }
+
+  File.open(filename, 'w') do |f|
+    f.puts "# Pull Requests"
+    f.puts
+    put_markdown_table(f, headings, rows, :age_SORT_KEY, false)
+  end
+  puts "Wrote #{filename}"
 end
 
 #################################
